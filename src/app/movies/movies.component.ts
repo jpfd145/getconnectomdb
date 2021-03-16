52a2b8a8e1f61a01019cea6e013de0e27cddd7ca
jpfd145 <jpfdias1119@gmail.com>
2021-03-16 13:09:30 -0300

    commit full application

---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { MoviesService } from './movies.service';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.css'],
+})
+export class MoviesComponent implements OnInit {
+  message: string;
+  movie: any;
+  movieFound: boolean;
+  moviePoster: string;
+
+  constructor(private moviesService: MoviesService) {}
+
+  ngOnInit(): void {}
+
+  searchMovies(term: string): void {
+    this.moviesService.search(term).subscribe((res) => {
+      if (res.Response === 'True') {
+        this.movie = res;
+        if (this.movie.Poster != 'N/A') {
+          this.moviePoster = this.movie.Poster;
+        } else {
+          this.moviePoster = '';
+        }
+        this.movieFound = true;
+      } else {
+        this.movieFound = false;
+        this.message = 'No movie was found that matched your search.';
+      }
+    });
+    console.log(term, this.movie);
+  }
+}
